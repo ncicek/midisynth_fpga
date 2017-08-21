df6965e42cfc38ee89a4eca9d1f9cc3b25db71b9
jdbens <ciceknebi@gmail.com>
2017-08-21 13:00:36 -0400

    update voicecontroller and tb

--- verilog/src/voice_controller.v
+++ verilog/src/voice_controller.v
@@ -1,61 +1,41 @@
-// `default_nettype none
+`default_nettype none
 module voice_controller(
-	input wire clk,
-	input wire reset,
+	input wire i_clk,
+	input wire i_reset,
 
-	input wire SPI_note_status,
-	input wire [7:0] SPI_voice_index,
-	input wire [6:0] SPI_midi_note,
-	input wire [6:0] SPI_velocity,
-	input wire SPI_ready_flag,
+	input wire i_SPI_note_status,
+	input wire [7:0] i_SPI_voice_index,
+	input wire [31:0] i_SPI_tuning_code,
+	input wire [6:0] i_SPI_velocity,
+	input wire i_SPI_ready_flag,
 
-	output reg signed [23:0] output_sample
+	output reg signed [23:0] o_mixed_sample
 	);
+
+	reg [7:0] voice_index;
 
 	//DDS////////////////////////////////////////
-	localparam  dds_data_width = 64;
-
-  reg [7:0] voice_counter;
-
-	wire [dds_data_width-1:0] dds_din;
-	reg [7:0] dds_addr = 8'b0;
-	reg dds_write_en = 1'b0;
-	wire [dds_data_width-1:0] dds_dout;
-
-	//memory bus bit assignments
-	wire [31:0] dds_dout_current_phase;
-	wire [31:0] dds_dout_delta_phase;
-	assign dds_dout_current_phase = dds_dout[31:0];
-	assign dds_dout_delta_phase = dds_dout[63:32];
-
-	reg [31:0] dds_din_current_phase = 32'b0;
-	reg [31:0] dds_din_delta_phase = 32'b0;
-	assign dds_din[31:0] = dds_din_current_phase;
-	assign dds_din[63:32] = dds_din_delta_phase;
-
+	wire [9:0] phase;
 	wire [7:0] dds_voice_index_next;
-	wire [9:0] dds_output_phase;
-	dds dds (.clk(clk),
-	.reset(reset),
-	.voice_index(voice_counter),
-	.dds_din(dds_din),
-	.dds_addr(dds_addr),
-	.dds_write_en(dds_write_en),
-	.dds_dout(dds_dout),
-	.output_phase(dds_output_phase),
-	.voice_index_next(dds_voice_index_next)
-	);
-	////////////////////////////////////////////////
+	reg [1:0] pipeline_state  ;
 
-	reg [3:0] wave_select = 4'd1;
+	dds dds (.i_clk(i_clk),
+	.i_reset(i_reset),
+	.i_SPI_flag(i_SPI_flag),
+	.i_SPI_tuning_code(i_SPI_tuning_code),
+	.i_SPI_voice_index(i_SPI_voice_index),
+	.i_voice_index(voice_index),
+	.i_pipeline_state(pipeline_state),
+	.o_phase(phase),
+	.o_voice_index_next(dds_voice_index_next)
+	);
+
+	//WAVETABLE///////////////////////////////////////////////
+	reg [3:0] wave_select;
 	wire signed [15:0] wavetable_output;
 	wire [7:0] wavetable_voice_index_next;
-	wavetable wavetable(.clk(clk),.reset(reset),.phase(dds_output_phase),.wave_select(wave_select),.sample(wavetable_output),.voice_index(dds_voice_index_next),.voice_index_next(wavetable_voice_index_next));
 
-
-
-
-
+	wavetable wavetable(.i_clk(i_clk),.i_reset(i_reset),.i_phase(phase),.i_wave_select(wave_select),.i_voice_index(dds_voice_index_next),.i_pipeline_state(pipeline_state),.o_voice_index_next(wavetable_voice_index_next),.o_sample(wavetable_output));
 
 	//ADSR////////////////////////////////////////////////////////////////////
 	reg[15:0] attack_amt;
@@ -63,152 +43,69 @@ module voice_controller(
 	reg [15:0] sustain_amt;
 	reg [15:0] rel_amt;
 
-	localparam adsr_data_width = 38;	//update this with the assignments below
+	wire signed [15:0] adsr_output;
 
-	wire [adsr_data_width-1:0] adsr_din;
-	reg [7:0] adsr_addr = 8'b0;
-	reg adsr_write_en;
-	wire [adsr_data_width-1:0] adsr_dout;
+	ADSR ADSR (
+    .i_clk(i_clk),
+    .i_reset(i_reset),
 
-	//data bus assignments
-	wire [32:0] adsr_dout_envelope;
-	wire [3:0] adsr_dout_state;
-	wire adsr_dout_keystate;
-	assign adsr_dout_envelope = adsr_dout[37:5];
-	assign adsr_dout_state = adsr_dout[4:1];
-	assign adsr_dout_keystate = adsr_dout[0];
+    .i_SPI_flag(i_SPI_flag),
+    .i_SPI_note_status(i_SPI_note_status),
+    .i_SPI_voice_index(i_SPI_voice_index),
 
-	reg [32:0] adsr_din_envelope = 33'b0;
-	reg [3:0] adsr_din_state = 4'b0;
-	reg adsr_din_keystate = 1'b0;
-	assign adsr_din[37:5] = adsr_din_envelope;
-	assign adsr_din[4:1] = adsr_din_state;
-	assign adsr_din[0] = adsr_din_keystate;
+    .i_voice_index(wavetable_voice_index_next),
+    .i_pipeline_state(pipeline_state),
+    .i_sample(wavetable_output),
 
-	wire signed [15:0] voice_chain_output;
+    .i_attack_amt(attack_amt),
+    .i_decay_amt(decay_amt),
+    .i_sustain_amt(sustain_amt),
+    .i_rel_amt(rel_amt),
 
-	ADSR ADSR (.clk(clk),
-	.reset(reset),
-	.voice_index(wavetable_voice_index_next),
-	.input_sample(wavetable_output),
-	.attack_amt(attack_amt),
-	.decay_amt(decay_amt),
-	.sustain_amt(sustain_amt),
-	.rel_amt(rel_amt),
-	.adsr_din(adsr_din),
-	.adsr_addr(adsr_addr),
-	.adsr_write_en(adsr_write_en),
-	.adsr_dout(adsr_dout),
-	.output_sample(voice_chain_output)
-	);
+    .o_sample(adsr_output)
+  );
 	////////////////////////////////////////////////////////////////////////
 
 
-	wire [31:0] tuning_code;
-	reg [6:0] midi_byte;
-	tuning_code_lookup tuning_code_lookup(.midi_byte(midi_byte),.tuning_code(tuning_code));
 
-  reg signed [23:0] mixer_buffer = 24'sd0;
-	reg [3:0] state;
-
-	reg [10-1:0] mem_phase [(1<<8)-1:0];//SIM ONLY
+  reg signed [23:0] mixer_buffer;
+	reg [1:0] pipeline_state;
 
 	always @(posedge clk) begin
-		if (reset == 1'b1) begin
-			state <= 4'b0;
+		if (reset) begin
+			pipeline_state <= 2'b0;
 			voice_counter <= 8'd0;
       mixer_buffer <= 24'sd0;
-			//midi_byte <= 7'b0;
 			wave_select <= 4'd1;
-
-      dds_addr <= 8'b0;
-      adsr_addr <= 8'b0;
-      dds_write_en <= 1'b0;
-      adsr_write_en <= 1'b0;
-      dds_din_current_phase <= 32'b0;
-      dds_din_delta_phase <= 32'b0;
-      adsr_din_envelope <= 33'b0;
-      adsr_din_state <= 4'b0;
-      adsr_din_keystate <= 1'b0;
 
       attack_amt <= 16'd10000;
       decay_amt <= 16'd10000;
       sustain_amt <= 16'd10000;
       rel_amt <= 16'd1000;
-
+			o_mixed_sample <= 24'sd0;
 		end
 		else begin
-			//defaults do not modify a read data row in ram
-			dds_din_delta_phase <= dds_dout_delta_phase;
-			dds_din_current_phase <= dds_dout_current_phase;
-			adsr_din_state <= adsr_dout_state;
-			adsr_din_keystate <= adsr_dout_keystate;
-			adsr_din_envelope <= adsr_dout_envelope;
+			if (pipeline_state < 2'd3)
+				pipeline_state <= pipeline_state + 1'b1;
+			else
+				pipeline_state <= 2'd0;
+		end
+	end
 
-			case (state)
-				0: begin
-					//idle state
-					state <= 4'd1;
-					dds_write_en <= 1'b0;
-					adsr_write_en <= 1'b0;
+	always @(posedge clk) begin
+		if (~reset) begin
+			if (pipeline_state == 2'd0)	//incrementor
+				voice_index <= voice_index + 1;
 
-				end
-				1: begin
-					dds_write_en <= 1'b0;
-					adsr_write_en <= 1'b0;
-					//increment voice counter
-					//check for rollover and handle new spi cmds
-					//JUST FOR SIM
-					mem_phase[voice_counter] <= dds_output_phase;
-					//END OF SIM STUFF
-
-					if (voice_counter == 8'hff) begin
-	          output_sample <= mixer_buffer + voice_chain_output;  //spit out a mixed sample
-	          mixer_buffer <= 24'sd0;   //clear the mixer buffer when voice counter is full to prepare for the next sample
-						voice_counter <= 8'h0;
-						state <= 4'd2;
-					end
-					else begin
-						voice_counter <= voice_counter + 1'b1;
-						mixer_buffer <= mixer_buffer + voice_chain_output;
-						state <= 4'd0;
-					end
-				end
-
-				//STATES 2 AND 3 PERFORM A READ-MODIFY-WRITE OP ON ALL MEMORIES
-				2: begin
-					//read all rams at the address of the spi cmd recieved
-					dds_write_en <= 1'b0;
-					adsr_write_en <= 1'b0;
-					//TODo need to syncronize these and probably put spi commands into a fifo
-					dds_addr <= SPI_voice_index;
-					adsr_addr <= SPI_voice_index;
-					state <= 4'd3;
-				end
-
-				3: begin
-					//update and write back all rams
-					dds_write_en <= 1'b1;
-					adsr_write_en <= 1'b1;
-					if (SPI_note_status == 1'b1) begin	//noteon
-						midi_byte = SPI_midi_note;	//blocking i think?
-						dds_din_delta_phase <= tuning_code;
-						adsr_din_keystate <= 1'b1;
-					end
-					else if (SPI_note_status == 1'b0) begin	//noteoff
-						adsr_din_keystate <= 1'b0;
-						//adsr keyoff
-					end
-
-					//copying this stuff over from state 1
-					//we need to keep voice counter action continous
-					voice_counter <= voice_counter + 1'b1;
-					mixer_buffer <= mixer_buffer + voice_chain_output;
-					mem_phase[voice_counter] <= dds_output_phase;
-
-					state <= 4'd0	;
-				end
-			endcase
+			else if (pipeline_state == 2'd2) begin	//mixer
+				if (voice_index == 8'hff) begin
+		      o_mixed_sample <= mixer_buffer + adsr_output;  //spit out a mixed sample
+		      mixer_buffer <= 24'sd0;   //clear the mixer buffer when voice counter is full to prepare for the next sample
+		    end
+		    else begin
+		      mixer_buffer <= mixer_buffer + adsr_output;
+		    end
+			end
 		end
 	end
 
--- verilog/tb/voice_controller_tb.v
+++ verilog/tb/voice_controller_tb.v
@@ -1,53 +1,59 @@
 `timescale 10ns / 10ns
-
+`default_nettype none
 module voice_controller_tb;
- reg clk;
- reg reset;
+	reg clk;
+	reg reset;
+	reg SPI_flag;
+	reg [31:0] SPI_tuning_code;
+	reg [7:0] SPI_voice_index;
+  reg SPI_note_status;
+  wire signed [23:0] mixed_sample;
+
+  voice_controller voice_controller(
+  	.i_clk(clk),
+  	.i_reset(reset),  
+  	.i_SPI_note_status(SPI_note_status),
+  	.i_SPI_voice_index(SPI_voice_index),
+  	.i_SPI_tuning_code(SPI_tuning_code),
+  	.i_SPI_velocity(),
+  	.i_SPI_ready_flag(SPI_flag),
+  	.o_mixed_sample(mixed_sample)
+  	);
+
+	always begin
+		#1 clk = !clk;
+	end
 
 
- reg SPI_note_status;
- reg [7:0] SPI_voice_index;
- reg [6:0] SPI_midi_note;
- reg [6:0] SPI_velocity;   	
- reg SPI_ready_flag;
- wire signed [24:0] output_sample;		
- voice_controller voice_controller (.clk(clk),
- 	.reset(reset),
- 	.SPI_note_status(SPI_note_status),
- 	.SPI_voice_index(SPI_voice_index),
- 	.SPI_midi_note(SPI_midi_note),
- 	.SPI_velocity(SPI_velocity),
- 	.SPI_ready_flag(SPI_ready_flag),
- 	.output_sample(output_sample)
- );
-
- always begin
-   #1 clk = !clk;
- end
+	initial
+	begin
+    //$dumpfile("dds_tb.vcd");
+    //$dumpvars;
+    //for (lp=0; lp < 256; lp = lp+1) $dumpvars(0, mem_output[lp]);
+    //$dumplimit(1000000000);
+		clk = 1;
+		reset = 1;
+		send_SPI = 0;
+		#10 reset = 0;
 
 
- initial
- begin
-   clk = 1;
-   reset = 1;		 
-   #10 reset = 0;
+    #4
+    SPI_flag = 1;
+    SPI_tuning_code = 20*1000000;
+    SPI_voice_index = 5;
+    SPI_note_status = 1'b1;
+    #2
+    SPI_flag = 0;
+    #10000000
+    SPI_flag = 1;
+    SPI_voice_index = 5;
+    SPI_note_status = 1'b0;
+    #2
+    SPI_flag = 0;
 
 
-   #20
-   SPI_voice_index = 1;
-   SPI_midi_note = 40;
-   SPI_note_status = 1;	 
-   SPI_ready_flag = 1;
-   //key_state = 1;
-   //#25
-   //key_state = 1;
+    //#100000000 $finish;
 
-
-
- end
-
-
-
-
+	end
 
 endmodule
